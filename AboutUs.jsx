4cbb6dc69b02a0648ff07a1747e3ce267e7ea5d3
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-10-30 13:15:41 -0400

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default (props) => (
+    <>
+        <h1>Our Values</h1>
+        <p>
+            Quality
+        </p>
+        <p>
+            Responsiveness
+        </p>
+        <p>
+            
+        </p>
+    </>
+);
---
+++ Protected.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+export default (props) => {
+    const history = useHistory();
+    if (!props.isLoggedIn) {
+        history.push('/');
+        return null;
+    } else {
+    return (
+        <>
+            {props.children}
+        </>
+    )
+}
+}
